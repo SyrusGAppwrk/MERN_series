6902e61ed450859d7117baec5c99197dd7a65cbb
Syrus <Syrus.g@appwrk.com>
2022-09-26 17:35:34 +0530

    show data from Mongo

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const {MongoClient} = require('mongodb')
+const url='mongodb://localhost:27017';
+const client = new MongoClient(url);
+const database ='dbSyrus';
+
+async function getProducts(){
+    let result = await client.connect();
+    let db =result.db(database)
+    let collection = db.collection('Product')
+    let response = await collection.find({}).toArray();
+    console.log(response)
+
+}
+
+getProducts();
